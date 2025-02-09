ec5476a26d286e7cfa6ed2d422b57b558b4e2883
spycat88 <spycat88@users.noreply.github.com>
2025-02-09 19:26:08 +0000

    emulationstation: bump package

--- packages/ui/emulationstation/package.mk
+++ packages/ui/emulationstation/package.mk
@@ -4,7 +4,7 @@
 # Copyright (C) 2024-present ROCKNIX (https://github.com/ROCKNIX)
 
 PKG_NAME="emulationstation"
-PKG_VERSION="b16761b42746590d429bbda4896b3bb6d5371b6e"
+PKG_VERSION="481d5374e31a0c42598e17c23381c63f05cd60ad"
 PKG_GIT_CLONE_BRANCH="master"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/ROCKNIX/emulationstation-next"
