80dabb59071ab3fbafafad6a33b3e0e3d00fca96
rmake <macebrooks@gmail.com>
2025-02-27 17:36:26 +0000

    Fix Odin 2 input mappings, update kernel 6.12.17 / 6.13.5

--- packages/kernel/linux/package.mk
+++ packages/kernel/linux/package.mk
@@ -27,11 +27,11 @@ case ${DEVICE} in
     PKG_URL="https://www.kernel.org/pub/linux/kernel/v${PKG_VERSION/.*/}.x/${PKG_NAME}-${PKG_VERSION}.tar.xz"
   ;;
   SM8550)
-    PKG_VERSION="6.13.4"
+    PKG_VERSION="6.13.5"
     PKG_URL="https://www.kernel.org/pub/linux/kernel/v${PKG_VERSION/.*/}.x/${PKG_NAME}-${PKG_VERSION}.tar.xz"
   ;;
   *)
-    PKG_VERSION="6.12.16"
+    PKG_VERSION="6.12.17"
     PKG_URL="https://www.kernel.org/pub/linux/kernel/v${PKG_VERSION/.*/}.x/${PKG_NAME}-${PKG_VERSION}.tar.xz"
     PKG_PATCH_DIRS+=" panfrost"
   ;;
